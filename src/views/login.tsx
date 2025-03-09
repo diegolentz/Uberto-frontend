d4d0bf2ba7b5800cff771fc3d57be205a34a5d7a
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-03-09 13:29:44 -0300

    formulario de reserva

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
+import { FormTravel } from "../components/formTravel.tsx/formTravel";
 
 export const Login = () => {
     return <>
         <Box sx={{border: '1px solid red', padding: '1rem'}}>
             <h1>Login</h1>
         </Box>
+        <FormTravel></FormTravel>
     </>
 };  
