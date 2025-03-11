51c48659f0d6e53449512622f8314f3b5957ec20
AdrianPerez0306 <perezadrian.m@hotmail.com>
2025-03-11 17:21:06 -0300

    Checkpoint push

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,9 +1,54 @@
-import { Box } from "@mui/material";
+import { Box, Input, styled, TextField } from "@mui/material";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+
+const LoginFormContainerBox = styled(Box)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+}));
+
+const StyledInputController = styled(TextField)(({ theme }) => ({
+    backgroundColor: theme.palette.secondary.main,
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+}));
+
+type Inputs = {
+    example: string,
+    exampleRequired: string,
+};
 
 export const Login = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const { control } = useForm({defaultValues:{firstName:''}});
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+    
     return <>
-        <Box sx={{border: '1px solid red', padding: '1rem'}}>
-            <h1>Login</h1>
-        </Box>
+
+        <LoginFormContainerBox>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <StyledInputController
+                    label='mock'
+                    // className="materialUIInput"
+                />
+
+                {/* register your input into the hook by invoking the "register" function */}
+
+                {/* <input defaultValue="test" {...register("example")} /> */}
+
+                {/* include validation with required or other standard HTML validation rules */}
+                {/* <input {...register("exampleRequired", { required: true })} /> */}
+                {/* errors will return when field validation fails  */}
+                {/* {errors.exampleRequired && <span>This field is required</span>} */}
+
+                <input type="submit" />
+            </form>
+        </LoginFormContainerBox>
     </>
 };  
