509758445d65b347c16267c2e5c98547e3a5f401
Diaz Matias Hernan <diaz.matiash@gmail.com>
2025-03-11 14:21:56 -0300

    componente: reactor de Recommendation, en home esta el caso de uso. Posee modo vista y modo edicion, guarda y cancela cambios

--- src/components/recommendation/recommendation.tsx
+++ src/components/recommendation/recommendation.tsx
@@ -1,6 +1,6 @@
-import {  useEffect, useState } from "react";
-import { CardRecom } from "./card-recom";
-import { CardRecomEdit } from "./card-recom-edit";
+import { Card, CardHeader, CardContent, Typography, Avatar, Box, TextField, Rating, Button } from "@mui/material"
+import StarIcon from "@mui/icons-material/Star"
+import { useEffect, useRef } from "react"
 
 export interface recommProps {
     name: string
@@ -9,45 +9,74 @@ export interface recommProps {
     comment: string
     avatarUrl: string
     isEdit: boolean
-    handle: () => void
+    editMode: boolean
 }
 
-export interface recommPropsEdit {
-    name: string
-    date: string
-    rating: number
-    comment: string
-    avatarUrl: string
-    isEdit: boolean
-    handle: () => void
-    handleCloseEdit: () => void
-}
+export const Recommendation = ({recom, handle} : {recom:recommProps, handle: (recom: recommProps) => void}) => {
 
-export const Recommendation = ({recom} : {recom:recommProps}) => {
-
-    console.log('dentro de recomendacion valor del isEdit => ', recom.isEdit)
-
-    //const [edit, setEdit] = useState(recom.isEdit)
+    const recomOrigin = useRef<recommProps>({...recom, editMode: false});
 
     useEffect(() => {
-        console.log("Nuevo estado de recom dentro de la recom", recom);
-        //console.log("Nuevo estado de edit dentro de la recom", edit);
-        //setEdit(recom.isEdit)
-    }, [recom]); 
+        // Solo establecer recomOrigin la primera vez
+        if (!recomOrigin.current) {
+            recomOrigin.current = {...recom, editMode: false};
+        }
+    }, []); 
 
-    const handleCloseEdit = () => {
-        recom.isEdit = false
-        recom.handle()
-        console.log('recom en cancelara ', recom)
+    const saveChange = async() => {
+        //pego al back los cambios
+        alert('guardo cambios en el back')
+        recom.editMode = false
+        handle({...recom, editMode:false})
     }
 
-    const handle = () => {
-        recom.handle()
+    const handleCloseEdit = () => {
+        console.log('recom original ', recomOrigin)
+        console.log('recom ', recom)
+        handle(recomOrigin.current)
     }
 
     return (
-            <>
-                {recom.isEdit ? <CardRecomEdit recom={{...recom, handleCloseEdit, handle}}/> : <CardRecom recom={recom}/> }
+        <>
+                <Card sx={{ maxWidth: 400, p: 2, borderRadius: 3, boxShadow: 3 }}>
+                <CardHeader
+                avatar={<Avatar src={recom.avatarUrl} alt={recom.name} />}
+                title={<Typography fontWeight="bold">{recom.name}</Typography>}
+                action={
+                    <Box display="flex" alignItems="center">
+                    {!recom.editMode && <StarIcon sx={{ color: "gold" }} />} 
+                    <Typography fontWeight="bold" ml={0.5}>
+                        {
+                            recom.editMode ? 
+                            <Rating value={recom.rating} 
+                            onChange={(_, newValue) => handle({...recom, rating:newValue || recom.rating})} 
+                            precision={0.5} size="large"/> : 
+                            recom.rating
+                        }
+                    </Typography>
+                    </Box>
+                }
+                />
+                <CardContent>
+                <Typography variant="body2" color="textSecondary">
+                    {recom.date}
+                </Typography>
+                    {
+                        recom.editMode ? 
+                        <TextField fullWidth multiline rows={3} label="Comentario" value={recom.comment} 
+                        onChange={(e) => handle({...recom, comment:e.target.value})} 
+                        sx={{ mt: 2 }}/> : 
+                        recom.comment
+                    }
+                </CardContent>
+                {
+                        recom.editMode ? 
+                        <Box component="section" sx={{display:"flex" ,justifyContent:"end" }}>
+                            <Button onClick={handleCloseEdit}>Cancelar</Button>
+                            <Button onClick={saveChange}>Guardar</Button>
+                        </Box> : <></>
+                    }
+                </Card> 
             </>
     )
 }
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -6,9 +6,9 @@ import { Recommendation, recommProps } from "../components/recommendation/recomm
 export const Home = () => {
     const {showToast} = useContext(msjContext)
 
-    const recomDelBack = {name:'Matias', date:'1/1/2025', rating:5 , comment:"comentario", avatarUrl:'avatar.jpeg', isEdit:false}
-    const [recom, setRecom] = useState(recomDelBack)
-    
+    const recomDelBack : recommProps = {name:'Matias', date:'1/1/2025', rating:5 , comment:"comentario", avatarUrl:'avatar.jpeg', isEdit:false, editMode: false}
+    const [recom, setRecom] = useState<recommProps>(recomDelBack)
+
     const cod200 = async () =>{
         try{
             const res = await axios.get('https://checkip.amazonaws.com')
@@ -29,21 +29,18 @@ export const Home = () => {
         }
     }
 
-    const handleChange = () =>{
-        console.log('llamo a handle de cambio con recom ', recom)
+    const handleChange = (recom: recommProps) =>{
         setRecom(recom)
     }
 
     const handleEdit = () =>{
-        console.log('recom antes', recom)
-        setRecom({...recom, isEdit:true})
-        console.log('recom ', recom)
+        setRecom({...recom, editMode:true})
     }
 
     return(
         <>
             <p>Home de Usuario</p>
-            <Recommendation recom={{...recom, handle:handleChange}}></Recommendation>
+            <Recommendation recom={recom} handle={handleChange}></Recommendation>
             {!recom.isEdit && <button onClick={handleEdit}>editar card recomendacion</button>}
             <button onClick={cod200}>cod 200</button>
             <button onClick={cod400}>cod 400</button>
