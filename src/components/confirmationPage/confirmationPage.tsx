c974f33e66ffcecb322590ea99079a89f1f877d7
PedroMcGeraghty <pedrogeraghty82@gmail.com>
2025-03-22 17:29:39 -0300

    se arregla un error de tipeo

--- src/components/confirmationPage/confirmationPage.tsx
+++ src/components/confirmationPage/confirmationPage.tsx
@@ -24,23 +24,31 @@ export const ConfirmationPage = ({ driver, travel }: { travel: FormDriver, drive
             const res = await passengerService.profileRatings(id)
             setRecommendation(res)
         }
-
     }
+
     useEffect(() => {
         recommended()
         console.log(travel.passengers)
-
     }, [])
-
-
 
     return (
         <>
-            <Typography sx={styles.title} >Confirm travel</Typography>
-            <Typography sx={styles.text} >Origin<Typography sx={styles.span}>{travel.origin}</Typography></Typography> {/* Aquí puedes poner el valor correspondiente */}
-            <Typography sx={styles.text}>Destiny<Typography sx={styles.span}>{travel.destination}</Typography></Typography>
-            <Typography sx={styles.text}>Date
-                <Typography sx={styles.span}>
+            <Typography sx={styles.title}>Confirm travel</Typography>
+            <Typography sx={styles.text}>
+                Origin
+                <Typography sx={styles.span} component="span">
+                    {travel.origin}
+                </Typography>
+            </Typography>
+            <Typography sx={styles.text}>
+                Destiny
+                <Typography sx={styles.span} component="span">
+                    {travel.destination}
+                </Typography>
+            </Typography>
+            <Typography sx={styles.text}>
+                Date
+                <Typography sx={styles.span} component="span">
                     {new Date(travel.date).toLocaleDateString('es-AR', {
                         year: 'numeric',
                         month: 'long',
@@ -48,29 +56,50 @@ export const ConfirmationPage = ({ driver, travel }: { travel: FormDriver, drive
                     })}
                 </Typography>
             </Typography>
-            <Typography sx={styles.text}>Duration<Typography sx={styles.span}>60</Typography></Typography>
-            <Typography sx={styles.text}>Number of passengers<Typography sx={styles.span}>{travel.passengers}</Typography></Typography>
-            <Divider></Divider>
+            <Typography sx={styles.text}>
+                Duration
+                <Typography sx={styles.span} component="span">
+                    {60}
+                </Typography>
+            </Typography>
+            <Typography sx={styles.text}>
+                Number of passengers
+                <Typography sx={styles.span} component="span">
+                    {travel.passengers}
+                </Typography>
+            </Typography>
+            <Divider />
             <Typography sx={styles.title}>Driver Premium</Typography>
-            <Typography sx={styles.text}>Name <Typography sx={styles.span}>{driver.name}</Typography ></Typography>
-            <Typography sx={styles.text}>Car <Typography sx={styles.span}>{driver.model}</Typography ></Typography>
-            <Typography sx={styles.text}>Patent <Typography sx={styles.span}>{driver.patent}</Typography ></Typography>
-            <Typography sx={styles.text}>Rating <Typography sx={styles.span}>5</Typography ></Typography>
+            <Typography sx={styles.text}>
+                Name
+                <Typography sx={styles.span} component="span">
+                    {driver.name}
+                </Typography>
+            </Typography>
+            <Typography sx={styles.text}>
+                Car
+                <Typography sx={styles.span} component="span">
+                    {driver.model}
+                </Typography>
+            </Typography>
+            <Typography sx={styles.text}>
+                Patent
+                <Typography sx={styles.span} component="span">
+                    {driver.patent}
+                </Typography>
+            </Typography>
+            <Typography sx={styles.text}>
+                Rating
+                <Typography sx={styles.span} component="span">
+                    {5}
+                </Typography>
+            </Typography>
             <Box margin={2} marginBottom={10}>
                 {recommendation?.map((reco, index) =>
-                    (
-                        <RecommendationCard key={index} recom={reco} handle={recommended}></RecommendationCard>
-
-                    )
+                    <RecommendationCard key={index} recom={reco} handle={recommended}></RecommendationCard>
                 )}
-
             </Box>
-            <ButtonConfirmation 
-                //goTo={function (): void {throw new Error("Function not implemented.")}} 
-            >
-
-            </ButtonConfirmation>
-
+            <ButtonConfirmation />
         </>
     )
 }
