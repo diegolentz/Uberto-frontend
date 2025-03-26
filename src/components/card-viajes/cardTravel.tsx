2cf67678e49bb06e34de77573810aadcf1942fbb
diegolentz <diegoolentz@gmail.com>
2025-03-26 15:42:24 -0300

    se pone el buton de la card de travel

--- src/components/card-viajes/cardTravel.tsx
+++ src/components/card-viajes/cardTravel.tsx
@@ -18,6 +18,7 @@ export const CardTravel = ({ value }: { value: TravelCard }) => {
   const recomEmpty: Recommendation = new Recommendation(value.id, '', new Date, 0, '', 0, '', true, true, '', '')
   const [flag, setFlag] = useState(false)
 
+
   const handleCreate = () => {
     setFlag(!flag)
   }
@@ -26,73 +27,76 @@ export const CardTravel = ({ value }: { value: TravelCard }) => {
   return (
     <Card sx={styles.cardBodyStyle} >
       <CardHeader
-        sx={styles.cardHeaderStyle}
-        title={
-          <Box>
-            <Typography sx={styles.userNameLastnameStyle}>
-              {value.passengerName}
-            </Typography>
-            <Box sx={styles.iconUserStyle}>
-              <GroupIcon sx={{ fontSize: '1.2rem' }} />
-              <Typography sx={styles.rateStyle}>
-                {value.numberPasaenger}
-              </Typography>
-            </Box>
-          </Box>
-        }
-        action={
-          <Box sx={styles.actionStyle}>
-            <Avatar sx={styles.imgUserStyle} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          </Box>
-        }
+      sx={styles.cardHeaderStyle}
+      title={
+        <Box>
+        <Typography sx={styles.userNameLastnameStyle}>
+          {value.passengerName}
+        </Typography>
+        <Box sx={styles.iconUserStyle}>
+          <GroupIcon sx={{ fontSize: '1.2rem' }} />
+          <Typography sx={styles.rateStyle}>
+          {value.numberPasaenger}
+          </Typography>
+        </Box>
+        </Box>
+      }
+      action={
+        <Box sx={styles.actionStyle}>
+        <Avatar sx={styles.imgUserStyle} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </Box>
+      }
       />
       <CardContent sx={styles.boxInfoStyle}>
-        <Box sx={styles.boxInfoTravelStyle}>
-          <Box>
-            <Typography sx={styles.dataTravelStyle}>
-              {'Origin'}
-            </Typography>
-            <Typography sx={styles.dataTravelStyle}>
-              {'Destination'}
-            </Typography>
-            <Typography sx={styles.dataTravelStyle}>
-              {'Date'}
-            </Typography>
-            <Typography sx={styles.priceTravelStyle}>
-              {'Price'}
-            </Typography>
-          </Box>
-          <Box sx={styles.infoTravelStyle}>
-
-            <Typography sx={styles.infoTravelStyle}>
-              {value.origin}
-            </Typography>
-
-            <Typography sx={styles.infoTravelStyle}>
-              {value.destination}
-            </Typography>
-
-            <Typography sx={styles.infoTravelStyle}>
-              {
-                `${utils.setDate(value.date)} |
-                     ${utils.setStartTime(value.date)} -
-                     ${utils.setEndTime((value.date), (value.duration))}hs`
-              }
-            </Typography>
-
-            <Typography sx={styles.priceTravelStyle}>
-              {`$ ${value.price}`}
-            </Typography>
-
-          </Box>
+      <Box sx={styles.boxInfoTravelStyle}>
+        <Box>
+        <Typography sx={styles.dataTravelStyle}>
+          {'Origin'}
+        </Typography>
+        <Typography sx={styles.dataTravelStyle}>
+          {'Destination'}
+        </Typography>
+        <Typography sx={styles.dataTravelStyle}>
+          {'Date'}
+        </Typography>
+        <Typography sx={styles.priceTravelStyle}>
+          {'Price'}
+        </Typography>
         </Box>
+        <Box sx={styles.infoTravelStyle}>
+
+        <Typography sx={styles.infoTravelStyle}>
+          {value.origin}
+        </Typography>
+
+        <Typography sx={styles.infoTravelStyle}>
+          {value.destination}
+        </Typography>
+
+        <Typography sx={styles.infoTravelStyle}>
+          {
+          `${utils.setDate(value.date)} |
+             ${utils.setStartTime(value.date)} -
+             ${utils.setEndTime((value.date), (value.duration))}hs`
+          }
+        </Typography>
+
+        <Typography sx={styles.priceTravelStyle}>
+          {`$ ${value.price}`}
+        </Typography>
+
+        </Box>
+      </Box>
       </CardContent>
 
-      {value.date < new Date() && (
-        <>
-          <Button onClick={handleCreate}>Calificar</Button>
-          {flag && <RecommendationCard recom={recomEmpty} handle={handleCreate}></RecommendationCard>}
-        </>
+      {new Date(value.date) < new Date() && (
+      <>
+
+        <Button onClick={handleCreate}>Calificar</Button>
+        {flag && <RecommendationCard recom={recomEmpty} handle={handleCreate}></RecommendationCard>}
+
+
+      </>
       )}
     </Card>
   );
