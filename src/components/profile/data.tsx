a5ec7e7ea2070e744f74380aa95a2ff542ac861d
diegolentz <diegoolentz@gmail.com>
2025-03-25 23:57:29 -0300

    perfil trae ok la info

--- src/components/profile/data.tsx
+++ src/components/profile/data.tsx
@@ -2,7 +2,7 @@ import { Box, Divider } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
 // Removed unused import of useForm
 import { AxiosError } from "axios";
-import {  DriverProfile } from "../../domain/driver";
+import { DriverProfile } from "../../domain/driver";
 import { passengerProfile, PassengerProfile } from "../../domain/passenger";
 import { driverService } from "../../services/driver.service";
 import { passengerService } from "../../services/passenger.service";
@@ -14,34 +14,32 @@ import { msjContext } from "../viewLayout/viewLayout";
 export const Data = () => {
     const id = parseInt(sessionStorage.getItem('userId')!);
     const isDriver = sessionStorage.getItem('role') === 'driver';
-    const [profile, setProfile] = useState<DriverProfile | PassengerProfile >(isDriver ? {} as DriverProfile : {} as PassengerProfile);
+    const [profile, setProfile] = useState<DriverProfile | PassengerProfile>(isDriver ? {} as DriverProfile : {} as PassengerProfile);
     const { showToast } = useContext(msjContext)
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const setChanges = (data: any) => {
         setProfile({ ...profile, ...data });
     }
+    const fetchForm = async () => {
+        try {
+            if (isDriver) {
+                const response = await driverService.getProfile(id);
+                console.log(response);
+                setProfile(response);
+            } else {
+                const response = await passengerService.getProfile(id);
+                console.log(response);
+                setProfile(response);
+            }
+        } catch (e: unknown) {
+            showToast((e as AxiosError<unknown>).response!);
+        }
+    };
 
 
-    
 
     useEffect(() => {
-        const fetchForm = async () => {
-            try {
-                if (isDriver) {
-                    const response = await driverService.getProfile(id);
-                    console.log(response);
-                    setProfile(response);
-                } else {
-                    const response = await passengerService.getProfile(id);
-                    console.log(response);
-                    setProfile(response);
-                }
-            } catch (e: unknown) {
-                showToast((e as AxiosError<unknown>).response!);
-            }
-        };
-
         fetchForm();
     }, []);
 
@@ -51,15 +49,15 @@ export const Data = () => {
             <Box sx={{ padding: '2rem 1rem 3rem 1rem' }}>
                 <ProfileForm entity={profile} func={setChanges} />
                 <Divider aria-hidden="true" sx={{ borderColor: '#a737fc' }} />
-                {/* {isDriver && (
+                {!isDriver && (
                     <>
                         <MoneyForm money={(profile as PassengerProfile).money} id={id} func={setChanges} />
                         <Divider aria-hidden="true" sx={{ borderColor: '#a737fc' }} />
-                        <FriendsComponent  id={id} />
+                        <FriendsComponent id={id} />
                     </>
-                )} */}
+                )}
             </Box>
         </>
     );
-}; 
+};
 
--- src/components/profileForm/profileForm.tsx
+++ src/components/profileForm/profileForm.tsx
@@ -1,10 +1,9 @@
 import { Button, Divider, Stack, TextField } from "@mui/material";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { estilosInput } from "../homeForm/homeFormStyles";
 import { DriverProfile } from "../../domain/driver";
 import { PassengerProfile } from "../../domain/passenger";
-import { passengerService } from "../../services/passenger.service";
 import { msjContext } from "../viewLayout/viewLayout";
 import { AxiosError } from "axios";
 
@@ -13,31 +12,13 @@ interface ProfileFormProps {
     func: (data: unknown) => void;
 }
 
-
 export const ProfileForm = ({ entity, func }: ProfileFormProps) => {
     const isDriver = sessionStorage.getItem('role') === 'driver';
-
-    const [profile, setProfile] = useState<DriverProfile | PassengerProfile>(entity);
-    const { showToast } = useContext(msjContext)
-    const setChanges = (data: unknown) => {
-        func(data);
-
-    }
+    const { showToast } = useContext(msjContext);
+    const [form, setForm] = useState<DriverProfile | PassengerProfile | null>(null);
 
     const { register, handleSubmit, formState: { errors, isValid } } = useForm({
-        mode: "onChange", // Para validar en cada cambio
-        defaultValues: isDriver ? {
-            firstname: (profile as DriverProfile).firstname,
-            lastname: (profile as DriverProfile).lastname,
-            price: (profile as DriverProfile).price,
-            domain: (profile as DriverProfile).domain,
-            brand: (profile as DriverProfile).brand,
-            model: (profile as DriverProfile).model,
-        } : {
-            firstname: (profile as PassengerProfile).firstname,
-            lastname: (profile as PassengerProfile).lastname,
-            cellphone: (profile as PassengerProfile).phone,
-        }
+        mode: "onChange",
     });
 
     const onSubmit = async (data: any) => {
@@ -47,147 +28,156 @@ export const ProfileForm = ({ entity, func }: ProfileFormProps) => {
         try {
             // const response = await passengerService.updateProfile(id, data)
             // showToast(response)
-            // setChanges(data)
+            // func(data)
         } catch (e: unknown) {
-            showToast((e as AxiosError<unknown>).response!)
-        };
-    }
+            showToast((e as AxiosError<unknown>).response!);
+        }
+    };
 
+    useEffect(() => {
+        setForm(entity);
+    },[]);
 
     return (
-        <>
-
-            <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', justifyContent: 'center', height: 'auto', margin: '1rem ' }}>
-                <Stack spacing={2} width={'80vw'}>
+        <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', justifyContent: 'center', height: 'auto', margin: '1rem ' }}>
+            <Stack spacing={2} width={'80vw'}>
+                <TextField
+                    size="small"
+                    label="Name"
+                    type="text"
+                    defaultValue={entity.firstname} 
+                    {...register("firstname", {
+                        required: "Este campo es obligatorio.",
+                        pattern: {
+                            value: /^[A-Za-z\s]+$/,
+                            message: "Solo se permiten letras.",
+                        },
+                        minLength: { value: 3, message: "Debe tener al menos 3 caracteres." },
+                        maxLength: { value: 15, message: "Debe tener como máximo 15 caracteres." }
+                    })}
+                    sx={estilosInput}
+                    error={!!errors.firstname}
+                    slotProps={{ inputLabel: { shrink: !!entity.firstname } }} 
+                    helperText={typeof errors.firstname?.message === "string" ? errors.firstname.message : ""}
+                />
+                <TextField
+                    size="small"
+                    label="Last Name"
+                    type="text"
+                    defaultValue={entity.lastname}
+                    {...register("lastname", {
+                        required: "Este campo es obligatorio.",
+                        pattern: {
+                            value: /^[A-Za-z\s]+$/,
+                            message: "Solo se permiten letras.",
+                        },
+                        minLength: { value: 3, message: "Debe tener al menos 3 caracteres." },
+                        maxLength: { value: 15, message: "Debe tener como máximo 15 caracteres." }
+                    })}
+                    sx={estilosInput}
+                    error={!!errors.lastname}
+                    slotProps={{ inputLabel: { shrink: !!entity.lastname } }} 
+                    helperText={typeof errors.lastname?.message === "string" ? errors.lastname.message : ""}
+                />
+                {isDriver ? (
+                    <>
+                        <TextField
+                            size="small"
+                            label="Base price"
+                            type="number"
+                            defaultValue={(entity as DriverProfile).price}
+                            {...register("price", {
+                                required: "Este campo es obligatorio.",
+                                valueAsNumber: true,
+                                validate: value => Number.isInteger(value) || "Debe ser un número entero.",
+                                min: { value: 100, message: "Debe ser un número entero positivo y mayor a 100." }
+                            })}
+                            sx={estilosInput}
+                            error={!!errors.price}
+                            slotProps={{ inputLabel: { shrink: !!entity.price } }} 
+                            helperText={typeof errors.price?.message === "string" ? errors.price.message : ""}
+                        />
+                        <Divider aria-hidden="true" sx={{ borderColor: '#a737fc' }} />
+                        <p>Automovilista Premium</p>
+                        <TextField
+                            size="small"
+                            label="Domain"
+                            type="text"
+                            defaultValue={(entity as DriverProfile).domain}
+                            {...register("domain", {
+                                required: "Este campo es obligatorio.",
+                                pattern: {
+                                    value: /^[A-Za-z0-9\s]+$/,
+                                    message: "Solo se permiten letras y números.",
+                                },
+                                minLength: { value: 6, message: "Debe tener al menos 6 caracteres." },
+                                maxLength: { value: 7, message: "Debe tener como máximo 7 caracteres." }
+                            })}
+                            sx={estilosInput}
+                            error={!!errors.domain}
+                            slotProps={{ inputLabel: { shrink: !!entity.domain } }} 
+                            helperText={typeof errors.domain?.message === "string" ? errors.domain.message : ""}
+                        />
+                        <TextField
+                            size="small"
+                            label="Brand"
+                            type="text"
+                            defaultValue={(entity as DriverProfile).brand}
+                            {...register("brand", {
+                                required: "Este campo es obligatorio.",
+                                pattern: {
+                                    value: /^[A-Za-z\s]+$/,
+                                    message: "Solo se permiten letras.",
+                                },
+                                minLength: { value: 3, message: "Debe tener al menos 3 caracteres." },
+                                maxLength: { value: 15, message: "Debe tener como máximo 15 caracteres." }
+                            })}
+                            sx={estilosInput}
+                            error={!!errors.brand}
+                            slotProps={{ inputLabel: { shrink: !!entity.brand } }} 
+                            helperText={typeof errors.brand?.message === "string" ? errors.brand.message : ""}
+                        />
+                        <TextField
+                            size="small"
+                            label="Model"
+                            type="number"
+                            defaultValue={(entity as DriverProfile).model}
+                            {...register("model", {
+                                required: "Este campo es obligatorio.",
+                                valueAsNumber: true,
+                                validate: value => Number.isInteger(value) || "Debe ser un número entero.",
+                                min: { value: 1990, message: "Debe estar entre 1990 y 2025." },
+                                max: { value: 2025, message: "Debe estar entre 1990 y 2025." }
+                            })}
+                            sx={estilosInput}
+                            error={!!errors.model}
+                            slotProps={{ inputLabel: { shrink: !!entity.model } }} 
+                            helperText={typeof errors.model?.message === "string" ? errors.model.message : ""}
+                        />
+                    </>
+                ) : (
                     <TextField
                         size="small"
-                        label="Name"
-                        type="text"
-                        {...register("firstname", {
+                        label="Phone"
+                        type="number"
+                        defaultValue={(entity as PassengerProfile).phone}
+                        {...register("cellphone", {
                             required: "Este campo es obligatorio.",
                             pattern: {
-                                value: /^[A-Za-z\s]+$/,
-                                message: "Solo se permiten letras.",
-                            },
-                            minLength: { value: 3, message: "Debe tener al menos 3 caracteres." },
-                            maxLength: { value: 15, message: "Debe tener como máximo 15 caracteres." }
+                                value: /^\d{10}$/,
+                                message: "Debe tener 10 dígitos numéricos."
+                            }
                         })}
                         sx={estilosInput}
-                        error={!!errors.firstname}
-                        helperText={typeof errors.firstname?.message === "string" ? errors.firstname.message : ""}
+                        error={!!errors.cellphone}
+                        slotProps={{ inputLabel: { shrink: !!entity.phone } }} 
+                        helperText={typeof errors.cellphone?.message === "string" ? errors.cellphone.message : ""}
                     />
-                    <TextField
-                        size="small"
-                        label="Last Name"
-                        type="text"
-                        {...register("lastname", {
-                            required: "Este campo es obligatorio.",
-                            pattern: {
-                                value: /^[A-Za-z\s]+$/,
-                                message: "Solo se permiten letras.",
-                            },
-                            minLength: { value: 3, message: "Debe tener al menos 3 caracteres." },
-                            maxLength: { value: 15, message: "Debe tener como máximo 15 caracteres." }
-                        })}
-                        sx={estilosInput}
-                        error={!!errors.lastname}
-                        helperText={typeof errors.lastname?.message === "string" ? errors.lastname.message : ""}
-                    />
-                    {!isDriver ? (
-                        <>
-                            <TextField
-                                size="small"
-                                label="Base price"
-                                type="number"
-                                {...register("price", {
-                                    required: "Este campo es obligatorio.",
-                                    valueAsNumber: true,
-                                    validate: value => Number.isInteger(value) || "Debe ser un número entero.",
-                                    min: { value: 100, message: "Debe ser un número entero positivo y mayor a 100." }
-                                })}
-                                sx={estilosInput}
-                                error={!!errors.price}
-                                helperText={typeof errors.price?.message === "string" ? errors.price.message : ""}
-                            />
-                            <Divider aria-hidden="true" sx={{ borderColor: '#a737fc' }} />
-                            <p>Automovilista Premium</p>
-                            <TextField
-                                size="small"
-                                label="Domain"
-                                type="text"
-                                {...register("domain", {
-                                    required: "Este campo es obligatorio.",
-                                    pattern: {
-                                        value: /^[A-Za-z0-9\s]+$/,
-                                        message: "Solo se permiten letras y números.",
-                                    },
-                                    minLength: { value: 6, message: "Debe tener al menos 6 caracteres." },
-                                    maxLength: { value: 7, message: "Debe tener como máximo 7 caracteres." }
-                                })}
-                                sx={estilosInput}
-                                error={!!errors.domain}
-                                helperText={typeof errors.domain?.message === "string" ? errors.domain.message : ""}
-                            />
-                            <TextField
-                                size="small"
-                                label="Brand"
-                                type="text"
-                                {...register("brand", {
-                                    required: "Este campo es obligatorio.",
-                                    pattern: {
-                                        value: /^[A-Za-z\s]+$/,
-                                        message: "Solo se permiten letras.",
-                                    },
-                                    minLength: { value: 3, message: "Debe tener al menos 3 caracteres." },
-                                    maxLength: { value: 15, message: "Debe tener como máximo 15 caracteres." }
-                                })}
-                                sx={estilosInput}
-                                error={!!errors.brand}
-                                helperText={typeof errors.brand?.message === "string" ? errors.brand.message : undefined}
-                            />
-                            <TextField
-                                size="small"
-                                label="Model"
-                                type="number"
-                                {...register("model", {
-                                    required: "Este campo es obligatorio.",
-                                    valueAsNumber: true,
-                                    validate: value => Number.isInteger(value) || "Debe ser un número entero.",
-                                    min: { value: 1990, message: "Debe estar entre 1990 y 2025." },
-                                    max: { value: 2025, message: "Debe estar entre 1990 y 2025." }
-                                })}
-                                sx={estilosInput}
-                                error={!!errors.model}
-                                helperText={typeof errors.model?.message === "string" ? errors.model.message : undefined}
-                            />
-                        </>
-                    ) : (
-                        <>
-                            <TextField
-                                size="small"
-                                label="Phone"
-                                type="number"
-                                {...register("cellphone", {
-                                    required: "Este campo es obligatorio.",
-                                    pattern: {
-                                        value: /^\d{10}$/,
-                                        message: "Debe tener 10 dígitos numéricos."
-                                    }
-                                })}
-                                sx={estilosInput}
-                                error={!!errors.cellphone}
-                                helperText={typeof errors.cellphone?.message === "string" ? errors.cellphone.message : undefined}
-                            />
-                        </>
-                    )}
-
-                    <Button type="submit" variant="contained" color="secondary" disabled={!isValid}>
-                        Save
-                    </Button>
-                </Stack>
-            </form>
-
-        </>
-    )
-
-}
+                )}
+                <Button type="submit" variant="contained" color="secondary" disabled={!isValid}>
+                    Save
+                </Button>
+            </Stack>
+        </form>
+    );
+};
